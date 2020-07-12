88fa5564c8be4b3785d3b538ddcdad83026544ad
Craig Pinto <craigpinto17@gmail.com>
2020-07-12 00:27:31 -0400

    Updated All Podcasts Page UI

--- resources/views/podcasts/all-podcasts.blade.php
+++ resources/views/podcasts/all-podcasts.blade.php
@@ -5,30 +5,44 @@
 @section('content')
 
   <div class="container-fluid" id="backgroundImg">
-    <div class="row">
+    {{-- <div class="row">
       <div class="mt-4">
         <h2 class="p-2" id="homeSectionHeader">&nbsp; ALL PODCASTS &nbsp;</h2>
       </div>
-    </div>    
+    </div> --}}
+
+    <div class="row py-3 mb-3">
+      <div class="w-100 d-flex mb-4">
+        <h2 class="allauths">ALL PODCASTS</h1>
+      </div>
+    </div>
 
     <div class="row">
-      @foreach ($podcasts as $podcast)
-        <div class="col-md-6" id="spacetop">
-            <a href="/podcasts/{{$podcast->slug}}" class="h-100 list-group-item list-group-item-action" id="card-main-hover" style="background-color:#f9faf5" >
-            <div class="row">
-              <div class="col-4">
-                <img class="w-100" src="/storage/Images/Podcasts/{{$podcast->profile_pic}}" alt="{{$podcast->name}}">
+
+      @foreach ($podcasts as $group)
+        <div class="col-md-6 mb-3">
+          <a href="/podcasts/{{$group->slug}}" class="list-group-item list-group-item-action h-100" id="card-main-hover">
+            <div class="d-flex flex-lg-row flex-md-column flex-sm-row flex-column">
+              <div class="d-flex justify-content-center mb-2">
+                <img class="pod-img-about" src="/storage/Images/Podcasts/{{$group->profile_pic}}" alt="{{$group->name}}">
               </div>
-              <div class="col-8" >
-                <h3 id="textRed"> <b> {{$podcast->name}} </b></h3>
-                <p style="color:#2e364f" >{{$podcast->description}}</p>
+              <div class="w-100 mt-0 d-sm-inline">
+                <h3 class="art-pod-title mb-0 text-center w-100" id="textRed">
+                  {{$group->name}}
+                </h3>
+                <div class="pl-sm-3 pr-sm-3">
+                  <p class="mb-0 text-justify">
+                    {{$group->description}}
+                  </p>
+                </div>
               </div>
             </div>
-            </a>
+          </a>
         </div>
       @endforeach
+
     </div>
-    <br>
+
   </div>
 
 @endsection
