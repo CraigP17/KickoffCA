5f13a16a81fc899298a270041a62a595ae948153
Craig Pinto <craigpinto17@gmail.com>
2020-07-14 23:50:25 -0400

    Updated All Sports Title UI

--- resources/views/sports/all.blade.php
+++ resources/views/sports/all.blade.php
@@ -6,31 +6,39 @@
 
 @section('content')
 
-  <div class="container" style="background-color:#f0f0f0;">
-    <div class="row" style="margin:0;padding:20px">
-        <h1 class="title is-2" style="width:100%;text-align:center">All Sports</h1>
-        <div class="row">
-          @foreach ($sports as $sport)
-            <div class="col-md-3 col-6" style="padding:10px;">
-              <a href="/sports/{{$sport->slug}}">
-                <div class="card" id="cardi">
-                  <img src="/storage/Images/Sports/{{$sport->img_logo}}" class="sport-logo" alt="{{$sport->name}}" style="text-align:center">
-                  <h3 style="text-align:center">{{$sport->name}}</h3>
-                </div>
-              </a>
-            </div>
-          @endforeach
-          @foreach ($sports as $sport)
-            <div class="col-md-3 col-6" style="padding:10px;">
-              <a href="/sports/{{$sport->slug}}">
-                <div class="card" id="cardi">
-                  <img src="/storage/Images/Sports/{{$sport->img_logo}}" class="sport-logo" alt="{{$sport->name}}" style="text-align:center">
-                  <h3 style="text-align:center">{{$sport->name}}</h3>
-                </div>
-              </a>
-            </div>
-          @endforeach
+  <div class="container-fluid" id="backgroundImg">
+    <div class="container" id="backgrey">
+
+      <div class="row py-3 mb-3">
+        <div class="w-100 d-flex mb-4">
+          <h2 class="allauths">ALL SPORTS</h1>
         </div>
+      </div>
+
+      <div class="row">
+        @foreach ($sports as $sport)
+          <div class="col-md-3 col-6 p-2">
+            <a href="/sports/{{$sport->slug}}">
+              <div class="card" id="cardi">
+                <img src="/storage/Images/Sports/{{$sport->img_logo}}" class="sport-logo" alt="{{$sport->name}}" style="text-align:center">
+                <h3 class="text-center py-auto">{{$sport->name}}</h3>
+              </div>
+            </a>
+          </div>
+        @endforeach
+        @foreach ($sports as $sport)
+          <div class="col-md-3 col-6" style="padding:10px;">
+            <a href="/sports/{{$sport->slug}}">
+              <div class="card" id="cardi">
+                <img src="/storage/Images/Sports/{{$sport->img_logo}}" class="sport-logo" alt="{{$sport->name}}" style="text-align:center">
+                <h3 style="text-align:center">{{$sport->name}}</h3>
+              </div>
+            </a>
+          </div>
+        @endforeach
+      </div>
+
+
     </div>
   </div>
 
