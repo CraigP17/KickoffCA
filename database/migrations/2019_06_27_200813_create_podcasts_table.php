7712a368cae6b0fc91b05b89731f1a85880009a2
Craig Pinto <craigpinto17@gmail.com>
2020-07-26 23:37:19 -0400

    Fresh migration of Podcasts, condensing table creation

--- database/migrations/2019_06_27_200813_create_podcasts_table.php
+++ database/migrations/2019_06_27_200813_create_podcasts_table.php
@@ -14,17 +14,50 @@ class CreatePodcastsTable extends Migration
     public function up()
     {
         Schema::create('podcasts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->bigIncrements('id');
-            $table->string('name')->unique();
+            $table->string('name')->unique()->nullable();
             $table->text('authors');
             $table->text('description');
-            $table->string('profile_pic')->default('default.png');
+            $table->string('dp_name')->default('default.png');
+            $table->string('dp_url');
             $table->string('sport')->nullable();
             $table->string('apple')->nullable();
             $table->string('spotify')->nullable();
-            $table->string('other')->nullable();
+            $table->string('other_name')->nullable();
+            $table->string('other_link')->nullable();
             $table->string('slug');
             $table->timestamps();
+        });
+
+        Schema::create('pods', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->bigIncrements('id');
+            $table->string('group_name');
+            $table->string('name');
+            $table->text('description');
+            $table->date('date');
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('group_name')->references('name')->on('podcasts');
+        });
+
+        Schema::create('clips', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->bigIncrements('id');
+            $table->string('group_name');
+            $table->string('name');
+            $table->string('sport');
+            $table->date('date');
+            $table->string('duration');
+            $table->string('link');
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('group_name')->references('name')->on('podcasts');
         });
     }
 
@@ -35,6 +68,10 @@ class CreatePodcastsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('pods');
+        Schema::dropIfExists('clips');
         Schema::dropIfExists('podcasts');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
